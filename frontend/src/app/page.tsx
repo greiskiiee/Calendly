018b5685167d004bdd95d91d9834e819c7910966
ochkojs <ochkojs@gmail.com>
2025-05-30 10:29:31 +0800

    FrontEnd main page, service card, header component

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,15 +1,17 @@
 import Image from "next/image";
 import { ServiceCartComponent } from "../../components/ServiceCard";
 import { MainAreaComponent } from "../../components/MainArea";
+import { HeaderComponent } from "../../components/Header";
 
 export default function Home() {
   return (
     <div className="flex flex-col gap-8">
+      <HeaderComponent />
       <MainAreaComponent />
       <div className="text-3xl font-bold text-center text-gray-800 mb-12">
         Танд тохирсон салоноо сонгоорой
       </div>
-      <div className="flex flex-wrap gap-8">
+      <div className="flex flex-wrap max-w-6xl mx-auto gap-8">
         <ServiceCartComponent />
         <ServiceCartComponent />
         <ServiceCartComponent />
