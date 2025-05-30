2ca9b1d104193101c19f229d84f395304c859c36
chimegerdene <chimgeebatuh@gmail.com>
2025-05-30 10:33:31 +0800

    done

---
+++ frontend/src/app/(auth)/login/page.tsx
@@ -0,0 +1,9 @@
+import SalonLogin from "@/components/SalonLogin";
+
+export default function Login() {
+  return (
+    <div className="w-screen">
+      <SalonLogin />
+    </div>
+  );
+}
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import { ServiceCartComponent } from "../../components/ServiceCard";
+import { ServiceCartComponent } from "../components/ServiceCard";
 
 export default function Home() {
   return <div>Main page</div>;
