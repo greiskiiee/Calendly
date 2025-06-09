c692893a32cf3892e9dd85f89fe72ae07432c458
24LP6774 24LP6774 <24LP6774@G305-employeeiMac.local>
2025-06-09 12:06:32 +0800

    FRONEND_URL change

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -12,7 +12,7 @@ const app = express();
 app.use(
   cors({
     credentials: true,
-    origin: 'http://localhost:3000',
+    origin: process.env.FRONTEND_URL || 'http://localhost:3000',
   })
 );
 app.use(json());
