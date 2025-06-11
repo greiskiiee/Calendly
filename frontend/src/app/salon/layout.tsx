7d2ec0a1cf29f927c62a725c1671668c8d8bfc5d
Chimegerdene Drl <Lenovo@DESKTOP-DG7QGI5.(none)>
2025-06-11 22:13:44 +0800

    salon layout

--- frontend/src/app/salon/layout.tsx
+++ frontend/src/app/salon/layout.tsx
@@ -1,29 +1,7 @@
-import { Geist, Geist_Mono } from "next/font/google";
-import "../globals.css";
-import { CompanyProvider } from "@/components/contexts/CompanyProvider";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
 export default function SalonLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
-  return (
-    <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        <div className="antialiased mx-auto">
-          <CompanyProvider>{children}</CompanyProvider>
-        </div>
-      </body>
-    </html>
-  );
+}) {
+  return <div>{children}</div>;
 }
