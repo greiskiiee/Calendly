4aa8c7c19852c7a4fee76fe00e2d98f54ca6190e
24LP6774 24LP6774 <24LP6774@G305-employeeiMac.local>
2025-06-13 12:42:20 +0800

    logo fix

--- frontend/src/components/Signup-step1.tsx
+++ frontend/src/components/Signup-step1.tsx
@@ -1,13 +1,13 @@
-"use client";
-import React, { ChangeEvent, useContext, useState } from "react";
+'use client';
+import React, { ChangeEvent, useContext, useState } from 'react';
 import {
   Select,
   SelectContent,
   SelectItem,
   SelectTrigger,
   SelectValue,
-} from "@/components/ui/select";
-import { Button } from "@/components/ui/button";
+} from '@/components/ui/select';
+import { Button } from '@/components/ui/button';
 import {
   Form,
   FormControl,
@@ -15,54 +15,54 @@ import {
   FormItem,
   FormLabel,
   FormMessage,
-} from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
-import { z } from "zod";
-import { useForm } from "react-hook-form";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { Eye, EyeOff } from "lucide-react";
-import { CompanyContext } from "./contexts/CompanyContext";
-import { uploadImage } from "@/app/utils/image-upload";
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { z } from 'zod';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { Eye, EyeOff } from 'lucide-react';
+import { CompanyContext } from './contexts/CompanyContext';
+import { uploadImage } from '@/app/utils/image-upload';
 
 type Props = {
   onContinue: () => void;
 };
 
 const categories = [
-  "Үсчин",
-  "Гоо сайхан",
-  "Спа",
-  "Эмчилгээний спа",
-  "Нүүр будалт",
+  'Үсчин',
+  'Гоо сайхан',
+  'Спа',
+  'Эмчилгээний спа',
+  'Нүүр будалт',
 ];
 
 const formSchema = z
   .object({
-    companyName: z.string().min(2, { message: "Нэрээ бүтэн бичнэ үү" }),
+    companyName: z.string().min(2, { message: 'Нэрээ бүтэн бичнэ үү' }),
     logo: z
       .any()
       .refine(
         (file) =>
-          ["image/jpeg", "image/jpg", "image/png", "image/webp"].includes(
+          ['image/jpeg', 'image/jpg', 'image/png', 'image/webp'].includes(
             file?.type
           ),
         {
-          message: "Only .jpg, .jpeg, .png and .webp formats are supported.",
+          message: 'Only .jpg, .jpeg, .png and .webp formats are supported.',
         }
       ),
-    email: z.string().email({ message: "Зөв имэйл оруулна уу" }),
-    phoneNumber: z.string().min(8, { message: "Зөв утасны дугаар оруулна уу" }),
+    email: z.string().email({ message: 'Зөв имэйл оруулна уу' }),
+    phoneNumber: z.string().min(8, { message: 'Зөв утасны дугаар оруулна уу' }),
     password: z.string().min(8, {
-      message: "Нууц үг 8-аас их тэмдэгттэй байх ёстой",
+      message: 'Нууц үг 8-аас их тэмдэгттэй байх ёстой',
     }),
     confirmPassword: z.string().min(8, {
-      message: "Нууц үг 8-аас их тэмдэгттэй байх ёстой",
+      message: 'Нууц үг 8-аас их тэмдэгттэй байх ёстой',
     }),
-    category: z.string().min(2, { message: "Төрлөө сонгоно уу" }),
+    category: z.string().min(2, { message: 'Төрлөө сонгоно уу' }),
   })
   .refine((data) => data.password === data.confirmPassword, {
-    message: "Нууц үг таарахгүй байна",
-    path: ["confirmPassword"],
+    message: 'Нууц үг таарахгүй байна',
+    path: ['confirmPassword'],
   });
 
 export const Step1 = ({ onContinue }: Props) => {
@@ -72,39 +72,39 @@ export const Step1 = ({ onContinue }: Props) => {
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
-      companyName: "",
-      logo: undefined,
-      email: "",
-      phoneNumber: "",
-      password: "",
-      confirmPassword: "",
-      category: "",
+      companyName: '',
+      logo: '',
+      email: '',
+      phoneNumber: '',
+      password: '',
+      confirmPassword: '',
+      category: '',
     },
   });
 
   const onSubmit = async (values: z.infer<typeof formSchema>) => {
-    console.log("working");
+    console.log('working');
     if (!file) {
-      console.log("Файл оруулаагүй байна");
+      console.log('Файл оруулаагүй байна');
       return;
     }
     const imageURL = await uploadImage(file);
-    console.log("imageURL:", imageURL);
+    console.log('imageURL:', imageURL);
     const newCompany = {
       companyName: values.companyName,
       logo: imageURL,
       email: values.email,
       phoneNumber: values.phoneNumber,
-      address: "",
+      address: '',
       password: values.password,
       category: values.category,
-      about: "",
+      about: '',
       socialUrls: [],
       schedule: {},
     };
-    console.log(newCompany, "nc");
+    console.log(newCompany, 'nc');
     setCompany(newCompany);
-    console.log("Submitted values:", company);
+    console.log('Submitted values:', company);
     onContinue();
   };
 
@@ -116,15 +116,15 @@ export const Step1 = ({ onContinue }: Props) => {
       setFile(event.target.files[0]);
     }
   };
-  console.log(file);
-  const handleOnClick = async (file?: File) => {
-    if (!file) {
-      console.log("file oruulaagu bn");
-      return;
-    }
-    const imageURL = await uploadImage(file);
-    console.log(imageURL);
-  };
+  // console.log(file);
+  // const handleOnClick = async (file?: File) => {
+  //   if (!file) {
+  //     console.log("file oruulaagu bn");
+  //     return;
+  //   }
+  //   const imageURL = await uploadImage(file);
+  //   console.log(imageURL);
+  // };
 
   return (
     <Form {...form}>
@@ -250,7 +250,7 @@ export const Step1 = ({ onContinue }: Props) => {
                     <div className="relative">
                       <Input
                         {...field}
-                        type={showPassword ? "text" : "password"}
+                        type={showPassword ? 'text' : 'password'}
                         className="w-full py-1 px-4 pr-10 rounded-md bg-white"
                         placeholder="Энд нууц үг оруулна уу"
                       />
@@ -284,7 +284,7 @@ export const Step1 = ({ onContinue }: Props) => {
                     <div className="relative">
                       <Input
                         {...field}
-                        type={showPassword ? "text" : "password"}
+                        type={showPassword ? 'text' : 'password'}
                         className="w-full py-1 px-4 pr-10 rounded-md bg-white"
                         placeholder="Энд нууц үг оруулна уу"
                       />
