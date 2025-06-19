93a17fec29e005f3236b30f7ee667715e5f5ea29
Chimegerdene Drl <Lenovo@DESKTOP-DG7QGI5.(none)>
2025-06-19 15:13:16 +0800

    img fix

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -3,8 +3,6 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { CompanyProvider } from "@/components/contexts/CompanyProvider";
 import { Company } from "@/components/contexts/CompanyContext";
-import { cookies } from "next/headers";
-import jwt from "jsonwebtoken";
 
 const geistSans = Geist({ variable: "--font-geist-sans", subsets: ["latin"] });
 const geistMono = Geist_Mono({
@@ -17,9 +15,6 @@ export default async function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-  const cookieStore = cookies();
-  const token = (await cookieStore).get("token")?.value;
-
   let initialCompany: Company = {
     companyName: "",
     logo: "",
@@ -32,15 +27,6 @@ export default async function RootLayout({
     schedule: {},
     socialUrls: [],
   };
-
-  if (token) {
-    try {
-      const decoded = jwt.verify(token, process.env.SECRET_KEY!) as Company;
-      initialCompany = decoded;
-    } catch (err) {
-      console.error("Invalid or expired token:", err);
-    }
-  }
 
   return (
     <html lang="en">
